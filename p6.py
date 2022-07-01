3c195469493b4c0e174946ae47de47f7aabb99af
subhash505 <mr.subhash505@gmail.com>
2022-07-01 05:30:25 +0530

    aded p6.

---
+++ p6.py
@@ -0,0 +1,12 @@
+dic1={1:10,2:20}
+print("dictionary_1 :",dic1)
+dic2={3:30,4:40}
+print("dictionary_1 :",dic2)
+dic3={5:50,6:60}
+print("dictionary_1 :",dic3)
+dic4={}
+dic4.update(dic1)
+dic4.update(dic2)
+dic4.update(dic3)
+#dic4=dic1+dic2+dic3
+print("Expected result: ",dic4)
